758b07c20e154353c0aee753a50bbd4b869deb06
isaac238 <isaac@isaac238.dev>
2023-10-26 18:23:24 +0100

    fixxxx

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -34,12 +34,18 @@ import DatabaseHandler from '~/utils/db_handler';
 	<header class="flex w-full justify-between bg-primary dark:bg-primary-dark p-2 dark:text-text-dark">
 		<NuxtLink to="/"><h1 class="font-bold text-2xl ml-2">Nuxt Blog</h1></NuxtLink>
 		<NuxtLink :to="'/' + profile.username" v-if="loggedIn" class="hidden md:flex justify-center justify-self-center items-center gap-1 cursor-pointer">
-			<NuxtImg :src="profile.avatar" class="w-7 h-7 rounded-3xl border-accent border-2"/> {{ profile.username }}</NuxtLink>
-		<div class="flex gap-2">
-		<NuxtLink v-if="loggedIn" to="/new" aria-label="New Post" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer"><Icon name="jam:write" /></NuxtLink>
-		<button  v-if="loggedIn" @click="Logout" aria-label="Logout" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer"><Icon name="mdi:logout" /></button>
-		<button v-if="loggedIn == false" @click="navigateToLogin" aria-label="Login" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer gap-1 text-sm"><Icon name="ic:round-login" />Login</button>
-		<ThemeSwitcher />
+			<NuxtImg :src="profile.avatar" class="w-7 h-7 rounded-3xl border-accent border-2"/> {{ profile.username }}
+		</NuxtLink>
+
+		<div v-if="loggedIn" class="flex gap-2">
+			<NuxtLink to="/new" aria-label="New Post" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer"><Icon name="jam:write" /></NuxtLink>
+			<button @click="Logout" aria-label="Logout" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer"><Icon name="mdi:logout" /></button>
+			<ThemeSwitcher />
+		</div>
+
+		<div v-if="loggedIn === false" class="flex gap-2">
+			<button @click="navigateToLogin" aria-label="Login" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer gap-1 text-sm"><Icon name="ic:round-login" />Login</button>
+			<ThemeSwitcher />
 		</div>
 	</header>
 	<span class="bg-accent w-full h-1 block" />

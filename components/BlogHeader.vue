58578b13308cd18b270d035e033c37caf86f5f98
isaac238 <isaac@isaac238.dev>
2023-10-26 14:40:06 +0100

    Hides username in header on small screens

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -32,7 +32,7 @@
 <template>
 	<header class="flex w-full justify-between bg-primary dark:bg-primary-dark p-2 dark:text-text-dark">
 		<NuxtLink to="/"><h1 class="font-bold text-2xl ml-2">Nuxt Blog</h1></NuxtLink>
-		<h1 v-if="loggedIn" class="flex justify-center justify-self-center items-center gap-1 cursor-default">
+		<h1 v-if="loggedIn" class="hidden md:flex justify-center justify-self-center items-center gap-1 cursor-default">
 			<NuxtImg :src="avatar" class="w-7 h-7 rounded-3xl border-accent border-2"/> {{ username }}</h1>
 		<div class="flex gap-2">
 		<NuxtLink v-if="loggedIn" to="/new" aria-label="New Post" class="p-2 border rounded-lg border-primary-dark bg-primary dark:bg-primary-dark hover:brightness-50 dark:border-primary flex items-center justify-center cursor-pointer"><Icon name="jam:write" /></NuxtLink>

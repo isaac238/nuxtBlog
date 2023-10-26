3e5b69196739a0df2111e42d20df3532de65a622
isaac238 <isaac@isaac238.dev>
2023-10-26 04:09:30 +0100

    Added profiles table update to confirm.

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 	const user = useSupabaseUser();
-	watch(user, () => {
+	const supabase = useSupabaseClient();
+	watch(user, async () => {
 	if (user.value) {
+		const userCount = await supabase.from('profiles').select('id', {count: 'exact', head: true}).eq('id', user.value.id);
+		if (userCount.count! <= 0 && userCount) {
+			try {
+				const updateObj = {
+				id: user.value.id,
+				username: user.value.user_metadata.user_name,
+				email: user.value.email,
+				avatar: user.value.user_metadata.avatar_url
+				};
+
+				const { error } = await supabase.from('profiles').upsert(updateObj);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		console.log("COUNT: " + userCount.count);
+
 		return navigateTo('/')  
 	}}, { immediate: true })
 </script>
